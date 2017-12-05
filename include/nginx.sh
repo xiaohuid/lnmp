00e706ed6ff58ca23b9bb17db22b6ea8fd22df85
Xiaohui <cvf@xiaohui.org>
2017-12-05 15:21:14 -0800

    Update nginx.sh

--- include/nginx.sh
+++ include/nginx.sh
@@ -3,7 +3,7 @@
 Install_Nginx_Openssl()
 {
     if [ "${Enable_Nginx_Openssl}" = 'y' ]; then
-        Download_Files ${Download_Mirror}/lib/openssl/${Openssl_Ver}.tar.gz ${Openssl_Ver}.tar.gz
+        Download_Files https://www.openssl.org/source/${Openssl_Ver}.tar.gz ${Openssl_Ver}.tar.gz
         [[ -d "${Openssl_Ver}" ]] && rm -rf ${Openssl_Ver}
         tar zxf ${Openssl_Ver}.tar.gz
         Nginx_With_Openssl="--with-openssl=${cur_dir}/src/${Openssl_Ver}"
